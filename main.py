da6891afab1a049cd51f44c89cc6fbe7fa7b3823
Ankit-rana <ankit9.7.91@gmail.com>
2016-06-12 18:50:22 +0530

    cleanup

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_restful import Api
-
 from base.user_data_handler import UserDataHandler
+
 
 app = Flask(__name__)
 api = Api(app)
-
 
 api.add_resource(UserDataHandler, '/user/<username>')
 
