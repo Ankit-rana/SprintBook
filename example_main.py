acc3e43a41e61957429b698543e620221d199482
ankit rana <anky@localhost.localdomain>
2016-07-25 09:47:02 +0530

    new branch for relational databases

--- example_main.py
+++ example_main.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask_restful import Api
 from base.user_data_handler import UserDataHandler
 
 
 app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(UserDataHandler, '/user/<username>')
+
+@app.route('/')
+def Sprint():
+   print "Sprint"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ utils/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.types import Date
+
+
+def _get_date():
+   return datetime.datetime.now()
+
+Base = declarative_base()
+
+
+class SprintBook(Base):
+   __tablename__ = 'sprintbook'
+   id = Column(Integer, primary_key = True )
+   name = Column(String(40)) 
+   user = Column(String(21))
+   
+
+class Sprint(Base):
+   __tablename__ = 'sprint'
+   id = Column(Integer, primary_key = True )
+   name = Column(String(40))
+   description = Column(String(100))
+   start_date = Column(Date, default = _get_date)
+   end_date = Column(Date, default=_get_date)
+   sprintbook = Column(ForeignKey(SprintBook.id))
+
+
+class Ticket(Base):
+   __tablename__ = 'ticket'
+   id = Column(Integer, primary_key = True)
+   name = Column(String(40))
+   sprint = Column(ForeignKey(Sprint.id))
+
+
+engine = create_engine('sqlite:///sprintbooks.db')
+Base.metadata.create_all(engine)
+
