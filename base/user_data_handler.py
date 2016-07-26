7aa5cf0c0f0a3e747d9b77785200e684f6749789
ankit rana <anky@localhost.localdomain>
2016-07-26 08:14:05 +0530

    endpoints created

--- base/user_data_handler.py
+++ base/user_data_handler.py
@@ -1,4 +1,3 @@
-from flask import request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from utils.models import Base, SprintBook 
--- example_main.py
+++ example_main.py
@@ -1,13 +1,23 @@
-from flask import Flask
-#from base.user_data_handler import UserDataHandler
+from flask import Flask,request
+from base.user_data_handler import UserDataHandler
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def getBook():
-   return "Sprint book recieved"
+def welcome_book():
+   return "Welcome to Sprint book"
+
+@app.route('/book/',method = ['GET','POST'])
+def book_interface():
+   if request.method == 'GET':
+      user = request.args.get('user','')
+      return UserDataHandler.get(user)
+   elif request.method == 'POST':
+      user = request.args.get('user','')
+      bookname = request.args.get('name','')
+      description = request.args.get('description','')
+      return UserDataHandler.put(user,bookname,description)
 
 
 if __name__ == "__main__":
